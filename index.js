c31c056a6182271cdc6d91d4944c9739bae97faa
Andrew MacIsaac <amacisaac95@gmail.com>
2023-01-17 00:07:55 -0700

    project complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(names){
+    return names.slice(0,2);
+}
+const returnLastTwoDrivers = function(names){
+    return names.slice(2,4);
+}
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+function createFareMultiplier(value){
+    return function (fare){
+        return fare * value;
+    }
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+function selectDifferentDrivers(names, funct){
+    let newnames = funct(names);
+    return newnames;
+}
